6e60d298e14f5641a00ddf24d5f3c944a4e739a5
trup98 <guru.gr998@gmail.com>
2024-02-13 18:57:16 +0530

    working on Oauth

---
+++ auth2/auth-client/src/main/java/com/springboot/authclient/config/WebConfig.java
@@ -0,0 +1,43 @@
+package com.springboot.authclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.servlet.config.annotation.*;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        super.addViewControllers(registry);
+        registry.addViewController("/").setViewName("forward:/index");
+        registry.addViewController("/index");
+        registry.addViewController("/secure");
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer configurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
---
+++ auth2/auth-client/src/test/java/com/springboot/authclient/AuthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.authclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
