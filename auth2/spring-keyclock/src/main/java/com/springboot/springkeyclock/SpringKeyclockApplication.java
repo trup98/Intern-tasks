6af2cd176a278e4e7295edc5647a48fd062878c0
trup98 <guru.gr998@gmail.com>
2024-02-20 16:26:50 +0530

    COMPLETED KeyClock

---
+++ auth2/spring-keyclock/src/main/java/com/springboot/springkeyclock/SpringKeyclockApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springkeyclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeyclockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKeyclockApplication.class, args);
+    }
+
+}
