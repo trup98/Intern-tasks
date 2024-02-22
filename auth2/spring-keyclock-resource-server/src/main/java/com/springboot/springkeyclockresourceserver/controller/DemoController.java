c9700b9bec9437fe68c0e66e2bac65da7f5843b7
trup98 <guru.gr998@gmail.com>
2024-02-22 18:48:02 +0530

    Configure Resource server with role based authentication

--- auth2/spring-keyclock-resource-server/src/main/java/com/springboot/springkeyclockresourceserver/controller/DemoController.java
+++ auth2/spring-keyclock-resource-server/src/main/java/com/springboot/springkeyclockresourceserver/controller/DemoController.java
@@ -1,13 +1,25 @@
 package com.springboot.springkeyclockresourceserver.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 
+@RestController
+@RequestMapping("/status")
 public class DemoController {
-    @GetMapping("/working")
-    public String demo() {
-        return "Working From Secure Server!";
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('admin')")
+    public String admin() {
+        return "Hello Admin !";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('user')")
+    public String user() {
+        return "Hello User !";
     }
 }
