5b4eeafc0e2656111661f9758a12d57e2cca3b22
trup98 <guru.gr998@gmail.com>
2024-03-14 12:32:48 +0530

    Partition Added in spring-batch

---
+++ spring-boot/spring-batch/src/main/java/com/springbatch/config/CustomerWriter.java
@@ -0,0 +1,23 @@
+package com.springbatch.config;
+
+import com.springbatch.entity.CustomerEntity;
+import com.springbatch.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CustomerWriter implements ItemWriter<CustomerEntity> {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public void write(List<? extends CustomerEntity> list) throws Exception {
+        System.out.println("Thread Name::::::"+ Thread.currentThread().getName());
+        customerRepository.saveAll(list);
+
+    }
+}
